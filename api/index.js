846da3ad6eda76ab2b00a5447f2a850ecb7b9693
sirdeeq <ssirdeeq@gmail.com>
2023-11-25 21:21:03 +0100

    error handling

--- api/index.js
+++ api/index.js
@@ -19,7 +19,6 @@ mongoose
 const app = express();
 app.use(express.json());
 
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!!");
 });
@@ -28,3 +27,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.use((err, res, req, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal  Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message
+  });
+});
